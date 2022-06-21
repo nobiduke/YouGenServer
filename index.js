cce6135da53d5d5851aa30f005d5ff7bdc8a0268
nobiduke <lmjones647@gmail.com>
2022-06-20 22:10:43 -0400

    started a basic API

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const PORT = 8080;
 
+var data = require('./data/dummy.json');
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// helper functions
+function isValidProf(prof){
+    console.log(prof.name);
+    if (!prof || !prof.name || !prof.capstone || !prof.description){
+        return false;
+    }
+    return true;
+}
+
+// requests
 app.get('/', (req, res)=>{
     res.send('Success');
+});
+
+app.get('/save', (req, res)=>{
+    let fs = require('fs');
+    fs.writeFile('./data/dummy.json', JSON.stringify(data), function(err){console.log(err);});
+    res.send('Saved State');
+});
+
+app.post('/addProf', urlencodedParser, (req, res)=>{
+    try{
+        let profile = JSON.parse(req.body.profile);
+        if(!isValidProf(profile)){
+            throw(`Invalid profile form ${profile}`);
+        }
+    } catch(err){
+        // console.log(err);
+        res.send(err);
+        return;
+    }
+    let profile = JSON.parse(req.body.profile)
+    profile['id'] = data.currentID;
+    data.currentID += 1;
+    data.profiles[profile.id] = profile;
+    res.send(`Added profile with name ${profile.name}`);
+});
+
+app.post('/reset', (req, res)=>{
+    let fs = require('fs');
+    fs.writeFile('./data/dummy.json', {profiles:{}, currentID:0}, function(err){console.log(err);});
+    res.send('Data cleared.');
 })
 
 app.listen(PORT, ()=>{
